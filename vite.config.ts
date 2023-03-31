c78b9574bea883387d7f052467b17b5cb6fc4f90
jjongminmoon <mjm7042@naver.com>
2023-03-31 22:05:46 +0900

    build commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     plugins: [react()],
+    base: "/mjstore/",
     resolve: {
       alias: [
         {
